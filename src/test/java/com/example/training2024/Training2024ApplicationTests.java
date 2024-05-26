beb9d7a2f5958a499e2cf41d7d4487ecc0e415fe
Wang <15027069102@163.com>
2024-05-26 13:23:10 +0800

    first commit

---
+++ src/test/java/com/example/training2024/Training2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.training2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Training2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
